8e47209ebf6c82ade5c6e12a18cb53196c1ed31f
wndeld777 <wndeld777@gmail.com>
2021-08-06 10:51:10 +0900

    내도서관

---
+++ App02_Library/app/src/main/java/com/wndeld777/library/MainActivity.java
@@ -0,0 +1,98 @@
+package com.wndeld777.library;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.wndeld777.library.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+
+    /**
+     * viewbinding 이 true 로 설정이 되면 자동으로 내부에서 생성되는 새로운 개념의 클래스이다
+     */
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /**
+         * setContentView(R.id.activity_main) 형식으로 *.xml 파일을 열어서
+         * 화면을 구성했던 코드가 다음과 같이 변경된다
+         */
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /**
+         * binding 이 성립되면 *.xml 에 포함된 View Component 는 별도의
+         * 객체 선언이 필요없고
+         * 사용할때는
+         *      binding.txt_name
+         *      binding.txt_message
+         *
+         *  와 같은 형식으로 직접 핸들리이 가능하다
+         */
+        setSupportActionBar(binding.toolbar);
+
+        // ActionBar 에 설정된 햄버거 버튼과 관련된 코드
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // fab = findByViewId(R.id.fab) 이런 코드가 필요했으나
+        // binding 이 성립된 경우는 필요없는 코드가 된다
+        // 직접 binding.fab 객체에 접근하여 event 등을 선언할 수 있게 된다
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
---
+++ App02_Library/app/src/main/java/com/wndeld777/library/config/Naver.java
@@ -0,0 +1,8 @@
+package com.wndeld777.library.config;
+
+public class Naver {
+
+    public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
+    public static String CLIENT_SECRET = "YOUR NAVER CLIENT SECRET";
+
+}
