34ee901f633d471965499fcad3d3c7f4c706d858
wndeld777 <wndeld777@gmail.com>
2021-08-10 09:47:27 +0900

    API

---
+++ App_03_Library/app/src/main/java/com/wndeld777/library/config/NaverAPI_Sample.java
@@ -0,0 +1,15 @@
+package com.wndeld777.library.config;
+
+/**
+ * Naver OpenAPI 에 접근하기 위한 정보들 포함하는 클래스
+ * Naver OpenAPI 관련 정보를 변경한 후
+ * NaverAPI 로 변경하여 프로젝트를 빌드하시오
+ */
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR NAVER API ID";
+    public static final String CLIENT_SECRET = "YOUR NAVER API SECRET";
+
+    public static final String NAVER_BOOK_URL = "https://openapi.naver.com/v1/search/book.json";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/"; // Retrofit
+}
