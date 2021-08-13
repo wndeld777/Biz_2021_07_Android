1eb2dfa000479e550de842efafe81e75c9bf4ed8
wndeld777 <wndeld777@gmail.com>
2021-08-13 09:38:22 +0900

    MyMovie

---
+++ project_my_movie/app/src/main/java/com/wndeld777/movie/config/NaverAPI_Sample.java
@@ -0,0 +1,10 @@
+package com.wndeld777.movie.config;
+
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR NAVER ID";
+    public static final String CLIENT_SECRET = "YOUR NAVER SECRET";
+
+    public static final String NAVER_BASE_URL = "";
+
+}
